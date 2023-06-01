0d42c8429d55986b8654819ff85a51aed59a80e4
ajinkya <ajinkyaraj@gmail.com>
2023-06-01 14:28:00 +0530

    Initial commit,test  example code from tokio-tunstenite for async websocket

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use tokio_tungstenite::connect_async;
+use futures_util::StreamExt;
+use url;
+#[tokio::main]
+async fn main()  {
+    let binance_path = String::from("wss://stream.binance.com:9443/ws/ethbtc@depth20@100ms");
+    let coinbase_path : String =
+    let url = url::Url::parse(&path).expect("Could not parse url");
+    let (ws_stream, _) = connect_async(url).await.expect("Could not connect to websocket");
+    let (_,mut read) = ws_stream.split();
+    loop {
+        let msg = read.next().await.unwrap();
+        println!("{:?}", msg);
+    }
+}
